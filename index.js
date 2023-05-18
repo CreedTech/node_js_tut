ba36ee9a7869afba05c6c4f65c892ece4cd55924
creedtech <ayodeleayolaoke4@gmail.com>
2023-05-18 13:12:54 +0100

    section-1 complete

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const tempOverview = fs.readFileSync(
   `${__dirname}/templates/overview.html`,
   'utf-8'
 );
+
 const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/product.html`,
@@ -48,6 +49,8 @@ const dataObject = JSON.parse(data);
 const slugs = dataObject.map((el) => slugify(el.productName));
 console.log(slugs);
 
+// !TODO
+// !FIXME
 console.log(slugify('Fresh Avocados', { lower: true }));
 
 const server = http.createServer((req, res) => {
